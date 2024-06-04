6587944688276d2e75d9de4e9dcd43deef95e797
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-06-05 02:05:49 +0530

    Issue #1: Create a application what parses xdocs with example config specified

---
+++ extractor/src/test/java/com/example/extractor/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.extractor;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        Main.main(new String[]{});
+        assertTrue(true);
+    }
+}
