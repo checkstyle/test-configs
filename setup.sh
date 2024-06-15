872737b44bfe4a853b3d3b06e9f90df20804f8e0
Roman Ivanov <ivanov-jr@mail.ru>
2024-06-15 16:43:31 -0700

    infra: speedup clone

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir -p .ci-temp
 
 # Clone the Checkstyle repository if it hasn't been cloned already
 if [ ! -d ".ci-temp/checkstyle" ]; then
-  git clone https://github.com/checkstyle/checkstyle.git .ci-temp/checkstyle
+  git clone --depth 1 --branch master https://github.com/checkstyle/checkstyle.git .ci-temp/checkstyle
 else
   echo "Checkstyle repository already cloned."
 fi
