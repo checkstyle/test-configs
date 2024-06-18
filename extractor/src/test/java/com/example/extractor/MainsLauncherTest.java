16f25f6658702e8733dfa77552786bc39932dba9
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-06-19 01:38:38 +0530

    Issue #1: Create a application what parses xdocs with example config specified

---
+++ extractor/src/test/java/com/example/extractor/MainsLauncherTest.java
@@ -0,0 +1,11 @@
+package com.example.extractor;
+
+import org.junit.jupiter.api.Test;
+
+public class MainsLauncherTest {
+
+    @Test
+    public void testMain() throws Exception {
+        Main.main(new String[]{});
+    }
+}
