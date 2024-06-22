6f929a00686bf8202200fe94ed6281023a5298ab
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-06-22 21:53:37 +0530

    Issue #1: Create a application what parses xdocs with example config specified

--- extractor/src/test/java/com/example/extractor/MainsLauncherTest.java
+++ extractor/src/test/java/com/example/extractor/MainsLauncherTest.java
@@ -7,9 +7,9 @@ public class MainsLauncherTest {
     @Test
     public void testMain() throws Exception {
         // Define the path in repo you want to test
-        String pathInRepo = "src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/abstractclassname";
+        String pathInRepo = "src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc";
         // Define the output directory
-        String outputDirectory = "AbstractClassName";
+        String outputDirectory = "SummaryJavadoc";
 
         // Pass the path and output directory as arguments
         Main.main(new String[]{pathInRepo, outputDirectory});
