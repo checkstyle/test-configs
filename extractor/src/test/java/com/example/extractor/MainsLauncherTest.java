a4bee26805cc877730fc382da96e31f0f2acc37f
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-06-29 22:56:37 +0530

    Issue #81: Resolve the issue for segemented Example files between Compilable and Non-compilable affecting the all-in-one

--- extractor/src/test/java/com/example/extractor/MainsLauncherTest.java
+++ extractor/src/test/java/com/example/extractor/MainsLauncherTest.java
@@ -3,22 +3,12 @@ package com.example.extractor;
 import org.junit.jupiter.api.Test;
 
 public class MainsLauncherTest {
-
     @Test
-    public void testMainTreeWalker() throws Exception {
-        // Define the path in repo you want to test
-        String pathInRepo = "src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming";
+    public void testMain() throws Exception {
+        // Define the base path to the Checkstyle repo
+        String checkstyleRepoPath = "../.ci-temp/checkstyle";
 
-        // Pass the path in repo as the argument
-        Main.main(new String[]{pathInRepo});
-    }
-
-    @Test
-    public void testMainNonTreeWalker() throws Exception {
-        // Define the path in repo you want to test for non-TreeWalker configuration
-        String pathInRepo = "src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/translation";
-
-        // Pass the path in repo as the argument
-        Main.main(new String[]{pathInRepo});
+        // Pass the base path as argument
+        Main.main(new String[]{checkstyleRepoPath});
     }
 }
