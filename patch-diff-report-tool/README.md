111ccb42f6c10d25395d684a6deaedaac1a2402f
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-07-21 22:22:20 +0530

    Issue #104: New Github workflow should not use contribution repository

---
+++ patch-diff-report-tool/README.md
@@ -0,0 +1,54 @@
+# patch-diff-report-tool
+
+A tool for creating a symmetrical difference from two checkstyle-result.xml files.
+It is useful when you have two different large checkstyle XML reports and want
+to know difference between them. It deletes identical lines present in both XML files,
+merges remaining lines into a result site.
+
+usage example:
+
+1) Create local copy of checkstyle/contribution repository with command
+   `git clone https://github.com/checkstyle/contribution`
+2) `cd ./contribution/patch-diff-report-tool`
+3) Compile source with `mvn clean install`, your application is located
+   in `cd ./contribution/patch-diff-report-tool/target` and
+   named `patch-diff-report-tool-0.1-SNAPSHOT-jar-with-dependencies.jar`
+
+You have 2 different checkstyle repos, original (base) and forked (patch), for each of them do 4-7:
+
+4) go to repo folder and execute in console `mvn clean install`
+5) go to `./contribution/checkstyle-tester` directory, uncomment all/required lines
+   in `projects-to-test-on.properties`, edit  `my_check.xml`
+6) execute `groovy diff.groovy --localGitRepo /home/johndoe/projects/checkstyle --baseBranch i111-my-fix
+   --patchBranch i111-my-fix-mutation --config config.xml --listOfProjects projects-to-test-on.properties`
+7) copy `checkstyle-result.xml` from `checkstyle-tester/reports/project-name/`
+   to some other location.
+
+8) Now execute this utility with 6 command line arguments:
+
+`--compareMode` - type of comparison to do with the files; `XML` parses the report files as
+   Checkstyle XML files with violation results. `TEXT` parses the report files as pure text
+   files, line by line. (required argument); default: XML \
+`--baseReport` - path to the base checkstyle-result.xml (optional argument,
+   if absent then only configuration and violations for patch will be in the report); \
+`--patchReport` - path to the patch checkstyle-result.xml (required argument); \
+`--refFiles` - path to the source files under check (optional argument); \
+`--output` - path to store the resulting diff report (optional argument,
+   default: ~/XMLDiffGen_report_yyyy.mm.dd_hh_mm_ss) \
+`--baseConfig` - path to the base checkstyle configuration xml file (optional argument); \
+`--patchConfig` - path to the patch checkstyle configuration xml file (optional argument); \
+`--shortFilePaths` - Option to save report file paths as a shorter version to prevent long paths.
+   This option is useful for Windows users where they are restricted to maximum directory depth. \
+`-h` - shows help message.
+
+Example:
+
+```bash
+java -jar ./patch-diff-report-tool-0.1-SNAPSHOT-jar-with-dependencies.jar \
+  --baseReport ~/contribution/checkstyle-tester/location1/checkstyle-result.xml \
+  --patchReport ~/contribution/checkstyle-tester/location2/checkstyle-result.xml \
+  --refFiles ~/contribution/checkstyle-tester/src/main/java \
+  --output ~/contribution/checkstyle-tester/site_result \
+  --baseConfig ~/contribution/checkstyle-tester/my_check.xml \
+  --patchConfig ~/contribution/checkstyle-tester/my_other_check.xml
+```
---
+++ patch-diff-report-tool/src/main/java/com/github/checkstyle/site/SiteGenerator.java
@@ -0,0 +1,229 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2024 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.github.checkstyle.site;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import com.github.checkstyle.Main;
+import com.github.checkstyle.data.CheckstyleRecord;
+import com.github.checkstyle.data.CliOptions;
+import com.github.checkstyle.data.DiffReport;
+import com.github.checkstyle.data.MergedConfigurationModule;
+import com.github.checkstyle.data.Statistics;
+
+/**
+ * Generates site report using thymeleaf template engine. Instead of single
+ * template 3 smaller ones are used with purpose of avoiding creation of extra
+ * large Context instance.
+ *
+ * @author attatrol
+ */
+public final class SiteGenerator {
+
+    /**
+     * Name for the site file.
+     */
+    public static final Path SITEPATH = Paths.get("index.html");
+
+    /**
+     * Pattern for a common file name beginning.
+     */
+    private static final Pattern COMMON_FILENAME_BEGINNING = Pattern.compile("src/main/java/.*");
+
+    /**
+     * Common filename beginning length.
+     */
+    private static final int COMMON_FILENAME_BEGINNING_LENGTH = 14;
+
+    /**
+     * Private ctor, please use generate method.
+     */
+    private SiteGenerator() {
+    }
+
+    /**
+     * Generates site report using thymeleaf template engine.
+     *
+     * @param diffReport
+     *        container with parsed data.
+     * @param diffConfiguration
+     *        merged configurations from both reports.
+     * @param options
+     *        CLI options.
+     * @throws IOException
+     *         on failure to write site to disc.
+     */
+    public static void generate(DiffReport diffReport, MergedConfigurationModule diffConfiguration,
+            CliOptions options) throws IOException {
+        // setup thymeleaf engine
+        final TemplateEngine tplEngine = getTemplateEngine();
+        // setup xreference generator
+        final XrefGenerator xrefGenerator = new XrefGenerator(options.getRefFilesPath(),
+                options.getOutputPath().resolve(Main.XREF_FILEPATH), options.getOutputPath());
+        // html generation
+        final Path sitepath = options.getOutputPath().resolve(SITEPATH);
+        final FileWriter writer = new FileWriter(sitepath.toString());
+        try {
+            // write statistics
+            generateHeader(tplEngine, writer, diffReport.getStatistics(), diffConfiguration);
+            // write parsed content
+            generateBody(tplEngine, writer, diffReport, options, xrefGenerator);
+            // write html footer
+            tplEngine.process("footer", new Context(), writer);
+        }
+        finally {
+            writer.close();
+        }
+    }
+
+    /**
+     * Creates thymeleaf template engine.
+     *
+     * @return template engine.
+     */
+    private static TemplateEngine getTemplateEngine() {
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setPrefix("/");
+        templateResolver.setSuffix(".template");
+        final TemplateEngine tplEngine = new TemplateEngine();
+        tplEngine.setTemplateResolver(templateResolver);
+        return tplEngine;
+    }
+
+    /**
+     * Creates beginning part of resulting site.
+     *
+     * @param tplEngine
+     *        thymeleaf template engine.
+     * @param writer
+     *        file writer.
+     * @param statistics
+     *        container for statistics.
+     * @param diffConfiguration
+     *        merged configurations from both reports.
+     */
+    private static void generateHeader(TemplateEngine tplEngine, FileWriter writer,
+            Statistics statistics, MergedConfigurationModule diffConfiguration) {
+        final Context context = new Context();
+        context.setVariable("statistics", statistics);
+        context.setVariable("config", diffConfiguration);
+        tplEngine.process("header", context, writer);
+    }
+
+    /**
+     * Creates main part of resulting site.
+     *
+     * @param tplEngine
+     *        thymeleaf template engine.
+     * @param writer
+     *        file writer.
+     * @param diffReport
+     *        difference between two checkstyle reports.
+     * @param options
+     *        CLI options.
+     * @param xrefGenerator
+     *        xReference generator.
+     */
+    private static void generateBody(TemplateEngine tplEngine, FileWriter writer,
+            DiffReport diffReport, CliOptions options, XrefGenerator xrefGenerator) {
+        final AnchorCounter anchorCounter = new AnchorCounter();
+
+        final Path refFilesPath = options.getRefFilesPath();
+        for (Map.Entry<String, List<CheckstyleRecord>> entry : diffReport.getRecords().entrySet()) {
+            final List<CheckstyleRecord> records = entry.getValue();
+            String filename = entry.getKey();
+
+            xrefGenerator.reset();
+
+            for (CheckstyleRecord checkstyleRecord : records) {
+                final String xreference = xrefGenerator.generateXref(checkstyleRecord.getXref(),
+                            options.isShortFilePaths());
+                checkstyleRecord.setXref(xreference);
+            }
+
+            if (refFilesPath != null) {
+                try {
+                    filename = refFilesPath.relativize(Paths.get(filename)).toString();
+                }
+                catch (IllegalArgumentException ignore) {
+                    // use original file name
+                }
+            }
+            generateContent(tplEngine, writer, records, shortenFilename(filename),
+                    anchorCounter);
+        }
+    }
+
+    /**
+     * Appends to the site a table with parsed data for a single file entry.
+     *
+     * @param tplEngine
+     *        thymeleaf template engine.
+     * @param writer
+     *        file writer.
+     * @param records
+     *        checkstyle records for a single file.
+     * @param filename
+     *        current file name from checkstyle reports.
+     * @param anchorCounter
+     *        anchor links provider.
+     */
+    private static void generateContent(TemplateEngine tplEngine, FileWriter writer,
+            List<CheckstyleRecord> records, String filename,
+            AnchorCounter anchorCounter) {
+        final Context context = new Context();
+        context.setVariable("filename", filename);
+        context.setVariable("records", records);
+        context.setVariable("anchor", anchorCounter);
+        tplEngine.process("content", context, writer);
+    }
+
+    /**
+     * Removes "src/main/java/" from filename beginning.
+     *
+     * @param filename
+     *        file name.
+     * @return shortened
+     *         file name.
+     */
+    private static String shortenFilename(String filename) {
+        final String shortenedFilename;
+        if (COMMON_FILENAME_BEGINNING.matcher(filename).matches()) {
+            shortenedFilename = filename.substring(COMMON_FILENAME_BEGINNING_LENGTH,
+                    filename.length());
+        }
+        else {
+            shortenedFilename = filename;
+        }
+        return shortenedFilename;
+    }
+
+}
