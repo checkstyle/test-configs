210305f8b6d4b6f3da0db451546c16330349f6c9
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2024-08-10 01:38:59 +0530

    Issue #153: New trigger comment for Workflow to reuse changed Inputs of Pull Request

---
+++ extractor/src/test/resources/com.puppycrawl.tools.checkstyle.checks.whitespace.methodparampad.InputFile/InputMethodParamPadWhitespaceAround.java
@@ -0,0 +1,15 @@
+/*
+MethodParamPad
+allowLineBreaks = (default)false
+option = SPACE
+tokens = (default)CTOR_DEF, LITERAL_NEW, METHOD_CALL, METHOD_DEF, SUPER_CTOR_CALL, \
+         ENUM_CONSTANT_DEF, RECORD_DEF, RECORD_PATTERN_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.methodparampad;
+
+public class InputMethodParamPadWhitespaceAround {
+
+}
